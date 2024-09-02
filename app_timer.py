956be3a547449a73054468accc898e803b31b2da
dark-ghost-123 <dark.ghost.12357@gmail.com>
2024-09-02 08:30:01 +0330

    add run cycle

--- app_timer.py
+++ app_timer.py
@@ -18,28 +18,24 @@ class Timer:
             "sublime_text.exe": [None, None],
             "chrome.exe": [None, None],
         }
+        self.counter = 0
 
+        self.file_setup()
+        self.app_list_cretor()
+        self.python_priority_changer()
+
+    def file_setup(self):
         with open("timerlog.csv", "a") as f:
             pass
         with open("timerlog.csv", "r") as f1:
             first_line = f1.readline()
-            print("first_line: ", first_line)
             if first_line != "app,shamsi,miladi,sclock,eclock,stime,etime,timer,reason\n":
                 lines = f1.readlines()
                 lines.insert(
                     0, "app,shamsi,miladi,sclock,eclock,stime,etime,timer,reason\n")
-                print("lines: ", lines)
                 with open("timerlog.csv", "w") as f2:
                     for line in lines:
                         f2.write(f"{line}")
-                    print("first line added")
-
-        self.app_list_cretor()
-        # print(self.app_name_list)
-        self.python_priority_changer()
-
-    def __str__(self):
-        return f"apps run on background: {self.app_list}"
 
     def app_list_cretor(self):
         self.app_list = set(app for app in psutil.process_iter())
@@ -58,10 +54,10 @@ class Timer:
     def app_exit(self, app: str):
         reason = self.reason_collector(app)
         with open("timerlog.csv", "a") as f:
-            # print(f"app,shamsi,miladi,sclock,eclock,stime,etime,timer,reason")
-            # print(f"{app},{JalaliDate.today().isoformat()},{datetime.date.today().isoformat()},{time.ctime(self.app_dict[app][0])},{time.ctime(self.app_dict[app][1])},{self.app_dict[app][0]},{self.app_dict[app][1]},{self.app_dict[app][1]-self.app_dict[app][0]},{reason}")
             print(
-                f"{app},{JalaliDate.today().isoformat()},{datetime.date.today().isoformat()},{datetime.datetime.fromtimestamp(self.app_dict[app][0]).strftime('%H:%M:%S.%f')},{datetime.datetime.fromtimestamp(self.app_dict[app][1]).strftime('%H:%M:%S.%f')},{self.app_dict[app][0]},{self.app_dict[app][1]},{int(self.app_dict[app][1]-self.app_dict[app][0])},{reason}", file=f)
+                f"{app},{JalaliDate.today().isoformat()},{datetime.date.today().isoformat()},{datetime.datetime.fromtimestamp(self.app_dict[app][0]).strftime('%H:%M:%S.%f')},{datetime.datetime.fromtimestamp(self.app_dict[app][1]).strftime('%H:%M:%S.%f')},{self.app_dict[app][0]},{self.app_dict[app][1]},{int(self.app_dict[app][1]-self.app_dict[app][0])},{reason}",
+                file=f
+            )
         self.app_dict[app][0] = None
         self.app_dict[app][1] = None
 
@@ -71,8 +67,10 @@ class Timer:
                 app.nice(psutil.REALTIME_PRIORITY_CLASS)
 
     def run(self):
+        print(f"{self.counter}")
         self.app_list_cretor()
         self.app_list_checker()
+        self.counter += 1
 
     def reason_collector(self, app):
         if app not in ["chrome.exe", "steam.exe", "gameup.exe"]:
@@ -89,5 +87,8 @@ class Timer:
 
 if __name__ == "__main__":
     timer = Timer()
-    for x in range(10):
-        timer.run()
+    schedule.every(5).seconds.do(timer.run)
+    
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
